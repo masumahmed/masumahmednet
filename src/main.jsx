d9bacc0da24e1795374dd36299eb5693087c37d7
Masum Ahmed <masumahmed1798@gmail.com>
2024-06-13 00:54:52 -0400

    main

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Projects from './pages/Projects'
 import Blog from './pages/Blog'
 import Photos from './pages/Photos'
 import BlogEntry from './pages/BlogEntry'
+import Contact from './pages/Contact'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -18,7 +19,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:title" element={<BlogEntry />} />
           <Route path="/photos" element={<Photos />} />
-
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1 style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>404</h1>} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>,
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function Contact() {
+    return <>
+        <div id="bodyWrapper">
+            <Header />
+        </div>
+        <Footer />
+    </>
+}
+
+export default Contact;
