87fc8bc03b6dbd621c5e9ee3f58970581af07c72
Masum Ahmed <masumahmed1798@gmail.com>
2024-06-12 23:33:38 -0400

    config base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  host: 'masumahmed.net',
+  base: '/masumahmednet/',
+  host: 'masumahmed.net'
 });
