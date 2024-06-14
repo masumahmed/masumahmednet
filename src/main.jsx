4b33598ac68944b3d1d8ef40fb67c4b630d68817
Masum Ahmed <masumahmed1798@gmail.com>
2024-06-14 04:39:44 -0400

    SPA

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -10,18 +10,29 @@ import Photos from './pages/Photos'
 import BlogEntry from './pages/BlogEntry'
 import Contact from './pages/Contact'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <BrowserRouter basename="">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:title" element={<BlogEntry />} />
-          <Route path="/photos" element={<Photos />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<h1 style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>404</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>,
-)
+function Main() {
+    useEffect(() => {
+        const pathname = new URLSearchParams(window.location.search).get('pathname');
+        if (pathname) {
+            window.history.replaceState({}, '', pathname);
+        }
+    }, []);
+
+    return <React.StrictMode>
+        <BrowserRouter basename="">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/blog/:title" element={<BlogEntry />} />
+                <Route path="/photos" element={<Photos />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<h1 style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>404</h1>} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+}
+
+export default Main;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />)
