59477d6044d5f46c44d3c1cede4dda176a545cfc
Masum Ahmed <masumahmed1798@gmail.com>
2024-06-15 00:51:28 -0400

    preview

--- src/pages/BlogEntry.jsx
+++ src/pages/BlogEntry.jsx
@@ -9,9 +9,9 @@ import BlogData from '../data/BlogData.json';
 
 function BlogEntry() {
     const [htmlContent, setHtmlContent] = useState('');
-    let { hash } = useParams();
+    const { hash } = useParams();
     
-    let date = '', author = '', md="", description = '', title = '';
+    let title = '', date = '', author = '', md="", description = '', img = '';
     BlogData.forEach(entry => {            
         if (entry.hash === hash) {
             title = entry.title;
@@ -19,6 +19,7 @@ function BlogEntry() {
             author = entry.author;
             md = entry.md;
             description = entry.description;
+            img = entry.img;
         }
     });
     
@@ -27,28 +28,24 @@ function BlogEntry() {
         const converter = new Showdown.Converter();
         for (let i = 0; i < md.length; i++)
             html += converter.makeHtml(md[i]);
-
-        // filter html for all empty <p> tags
         html = html.replace('<p><\/p>', '');
 
         setHtmlContent(html);
     }, []);
-
-
-    title = title.replace(/\b\w/g, l => l.toUpperCase());
-    title = title.replace('-', ' ');
 
     return <>
         <div id="bodyWrapper">
             <Header />
             <div className="content">
                 <div className="con border markdown" style={{ "borderBottom": "1px solid #3a3a3a" }}>
-                    <br />
                     {!date && <>
                         <h1 style={{ "textAlign": "center" }}>404</h1>
                         <h1 style={{ "textAlign": "center" }}>no such blog exists</h1>
                     </>}
                     {date && <>
+                        <img className="previewImage" src={img} />
+                        <br />
+                        <br />
                         <h1 style={{ "textAlign": "left" }}>{title}</h1>
                         <hr />
                         <p><i>{author} - {date} - {description}</i></p>
