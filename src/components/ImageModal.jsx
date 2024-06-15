3c1045ae7d1b21e9d03884542cdeac271b116283
Masum Ahmed <masumahmed1798@gmail.com>
2024-06-15 02:18:40 -0400

    preview

---
+++ src/components/ImageModal.jsx
@@ -0,0 +1,14 @@
+function ImageModal({ img }) {
+    
+    return <>
+        <div onClick={} className="" id="modal-container">
+            <p id="tip">click anywhere to close the image</p>
+        </div>
+
+        <div onClick={} className="" id="modal">
+            <img id="modal-image" src={img} alt="" />
+        </div>
+    </>
+}
+
+export default ImageModal;
