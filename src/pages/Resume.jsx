719c0e46e989970e14cf0606e29099aba795d12e
Masum Ahmed <masumahmed1798@gmail.com>
2024-06-19 00:55:30 -0400

    resume rout

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,4 @@
 function Resume() {
-    // if mobile client then redirect to https://docs.google.com/gview?embedded=true&url=<URL of a supported doc>
-    // else redirect to /resume.pdf
     return <>
         <meta httpEquiv='refresh' content='0; URL=/resume.pdf'/>
     </>
